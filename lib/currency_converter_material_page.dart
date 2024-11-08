0af3cf54256b28fa6ca160e3dacc9ac60440499b
mjm836 <mjm836>
2024-11-08 11:00:55 -0500

    moved page off of main

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget {
+  const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Hello World!",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
